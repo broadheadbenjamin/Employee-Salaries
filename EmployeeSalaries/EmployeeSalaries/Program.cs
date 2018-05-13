5307de8875ce3739abc4c7c8962d32bdd63a9f8f
Ben Broadhead <broadhead.benjamin@gmail.com>
2018-05-13 16:20:36 -0600

    Initial commit.
    Initial commit. Added readme and set up project.

---
+++ EmployeeSalaries/EmployeeSalaries/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeSalaries
+{
+    /// <summary>
+    /// Employee class.
+    /// </summary>
+    class Employee
+    {
+
+    }
+
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
